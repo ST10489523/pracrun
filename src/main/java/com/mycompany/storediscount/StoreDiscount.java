bb58c9a6516bcc6bc1c9c7ed1c577900ce9ac6e3
unknown <st10489523@vcconnect.edu.za>
2025-04-07 20:26:00 +0200

    Store Discount

--- src/main/java/com/mycompany/storediscount/StoreDiscount.java
+++ src/main/java/com/mycompany/storediscount/StoreDiscount.java
@@ -4,13 +4,28 @@
 
 package com.mycompany.storediscount;
 
-/**
- *
- * @author lab_services_student
- */
-public class StoreDiscount {
+import java.util.Scanner;
 
+public class StoreDiscount {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter the total purchase amount (in R): ");
+            double totalAmount = scanner.nextDouble();
+            double discount = 0;
+            
+            if (totalAmount > 500) {
+                if (totalAmount > 1000) {
+                    discount = 0.20;
+                } else {
+                    discount = 0.10;
+                }
+            } else {
+                discount = 0.0;
+            }
+            
+            double discountedAmount = totalAmount - (totalAmount * discount);
+            System.out.printf("Discount applied: %.0f%%\n", discount * 100);
+            System.out.printf("Final amount after discount: R%.2f\n", discountedAmount);
+        }
     }
 }
